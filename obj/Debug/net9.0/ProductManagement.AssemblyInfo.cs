221e13f3c675231ee2645d334c578c9e7919f2fc
Kishioue <Kishitoue@gmail.com>
2025-04-13 22:54:40 -0400

    commit

--- obj/Debug/net9.0/ProductManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e39ba0d05f9e4c0fce8991cedd5bf3384f96bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
